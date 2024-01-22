4fff956694591bb26922f14ab0d04e043fa4bc6b
lahaina <nattolecats@gmail.com>
2024-01-22 21:08:20 +0900

    umount: unbind odm directory before system umount

--- umount-all.sh
+++ umount-all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo umount output/system/
+sudo umount output/system/odm/
 sudo umount output/system/product/
 sudo umount output/system/system_ext/
 sudo umount output/system/vendor/
+sudo umount output/system/
 
 rm -rf output/system
